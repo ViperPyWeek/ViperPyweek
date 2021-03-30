ddbbbe786034b4f41e23a310c46823d4e01f5c94
Samisai <waliul.sami.ahmed@gmail,com>
2021-03-29 19:58:33 -0600

    Add font, better menus, splash screen

--- src/copsGame.py
+++ src/copsGame.py
@@ -1,16 +1,24 @@
 import pygame
 import sys
 import shapesHelpers
+import os
 
+# TODO make stuff look nicer, add blocks behind selection, make buttons change color when hovered, shooting star background
+print()
 pygame.init()
 window = pygame.display.set_mode((400, 400))  # creating pygame window
 pygame.display.set_caption("Space Force")
-mars = pygame.image.load(r"Repo\ViperPyweek\src\assets\mars.png")
+# The below assets will need to be changed to your own paths atm
+mars = pygame.image.load(r"Repo\ViperPyweek\src\assets\mars.png") 
+splashScreen = pygame.image.load(r"Repo\ViperPyweek\src\assets\splashScreen.png").convert()
+font = pygame.font.Font(r"Repo\ViperPyweek\src\assets\gameFont.ttf", 35)
 shapesHelpers.init(window)
-font = pygame.font.SysFont('Corbel', 35)
+#font = pygame.font.SysFont('Corbel', 35)
 menuDisp = True
 pauseDisp = False
+splashed = False
 white = (255, 255, 255)
+
 
 def drawPlanet(planetSprite):
     global window
@@ -18,9 +26,9 @@ def drawPlanet(planetSprite):
 
 def mainMenu():
     exitButton = font.render('Exit', True, white)
-    window.blit(exitButton , (250, 100))
+    window.blit(exitButton , (170, 250))
     startButton = font.render('Start', True, white)
-    window.blit(startButton , (75, 100))
+    window.blit(startButton , (160, 150))
 
 def pauseScreen():
     shapesHelpers.rect(0, 0, 400, 400, red=0, green=0, blue=0)
@@ -29,34 +37,48 @@ def pauseScreen():
     menuButton = font.render('Main Menu', True, white)
     window.blit(menuButton, (130, 250))
 
-while True:
-    pygame.time.wait(100)
+def splashScrDisp():
+    pressSpace = font.render('Press Space', True, white)
+    window.blit(splashScreen, (0, 0))
+    window.blit(pressSpace, (105, 300))
+
+def inputMgmt():
+    global pauseDisp, menuDisp, splashed
     for event in pygame.event.get():
+        keys = pygame.key.get_pressed()
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse = pygame.mouse.get_pos()
-            if 305 >= mouse[0] >= 250 and 100 <= mouse[1] <= 135 and menuDisp:
+            if 260 >= mouse[0] >= 150 and 250 <= mouse[1] <= 285 and menuDisp and splashed:
                 pygame.quit()
                 sys.exit()
-            elif 140 >= mouse[0] >= 75 and 100 <= mouse[1] <= 135 and menuDisp:
+            elif 260 >= mouse[0] >= 150 and 150 <= mouse[1] <= 185 and menuDisp and splashed:
                 menuDisp = False
             elif 260 >= mouse[0] >= 150 and 150 <= mouse[1] <= 185 and pauseDisp:
                 pauseDisp = False
             elif 260 >= mouse[0] >= 150 and 250 <= mouse[1] <= 285 and pauseDisp:
                 pauseDisp = False
                 menuDisp = True
-        elif event.type == pygame.KEYDOWN:
-            if not menuDisp and pygame.K_ESCAPE and pauseDisp:
-                pauseDisp = False
-            elif not menuDisp and pygame.K_ESCAPE:
-                pauseScreen()
-                pauseDisp = True
+        if keys[pygame.K_ESCAPE] and not menuDisp and pauseDisp:
+            pauseDisp = False
+        elif not menuDisp and keys[pygame.K_ESCAPE]:
+            pauseScreen()
+            pauseDisp = True
+        elif not splashed and keys[pygame.K_SPACE]:
+            splashed = True
         elif event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    if not pauseDisp:
-        shapesHelpers.rect(1, 1, 400, 400, red=0, green=0, blue=0)
-    if not menuDisp and not pauseDisp:
-        drawPlanet(mars)
-    if menuDisp:
-        mainMenu()
+
+while True: # gameloop
+    pygame.time.wait(100)
+    inputMgmt()
+    if not splashed:
+        splashScrDisp()
+    if splashed:
+        if not pauseDisp:
+            shapesHelpers.rect(1, 1, 400, 400, red=0, green=0, blue=0)
+        if not menuDisp and not pauseDisp:
+            drawPlanet(mars)
+        if menuDisp:
+            mainMenu()
     pygame.display.update()  # updates the display
